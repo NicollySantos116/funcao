c3559ff4aea937149ba472abafc3d2150e6dbd66
Nicolly <saantos.nick1606@gmail.com>
2025-05-30 14:50:19 -0300

    Missão Par ou Ímpar

---
+++ Ex02.js
@@ -0,0 +1,13 @@
+function  ehPar(numero){
+    if(numero % 2 == 0){
+        return "True";
+    } else{
+        return 'False';
+    }
+}
+
+
+console.log(ehPar(4));
+console.log(ehPar(17));
+console.log(ehPar(0));
+console.log(ehPar(-8));
