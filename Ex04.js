6131e694168b2d3794199d8c6a9c601eaa02f9ab
Nicolly <saantos.nick1606@gmail.com>
2025-05-30 16:13:42 -0300

    Feitiço da Espada Invertida

--- Ex04.js
+++
@@ -1,8 +0,0 @@
-function inverterString(str){
-    return str.split('').reverse().join('');
-}
-
-let stringOriginal= "JavaScript";
-let stringInvertida = inverterString(stringOriginal);
-console.log("String original: ", stringOriginal);
-console.log("String invertida: ", stringInvertida);
