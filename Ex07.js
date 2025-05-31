e8f80c5e5f39682145d48709fbeedc586f0cc36e
Nicolly <saantos.nick1606@gmail.com>
2025-05-30 21:52:24 -0300

    Filtro dos Guardiões dos Pares

--- Ex07.js
+++ Ex07.js
@@ -1,12 +1,14 @@
-function filtarPares(arr){
-    let Pares = [];
+function filtrarPares(arr){
+    let pares = [];
     for (let i = 0; i < arr.length; i++ ){
         if(arr[i] % 2 === 0){
-            Pares.push(arr[i]);
+            pares.push(arr[i]);
         }
     }
-    return Pares;
+    return pares;
 }
 
-console.log(filtarPares([1,2,3,4,5,6]));
-console.log(filtarPares([7,9,11]));
+console.log(filtrarPares([1,2,3,4,5,6]));
+console.log(filtrarPares([7,9,11]));
+console.log(filtrarPares([0,-2,-3,-4,5]));
+console.log(filtrarPares([10,21,32,43]));
